0315a0ee8c7cb7ba13791a251d29b8d3bc316374
evelynsini <evelynsini@users.noreply.github.com>
2026-05-21 19:58:02 -0600

    Add if-else exercises, switch, user input, and loops folders

---
+++ Chapter7_if_else/Student grade.js
@@ -0,0 +1,16 @@
+let mark=100;
+if(mark>=90){
+    console.log("Grade A")
+}
+else if(mark>=80)
+    {
+    console.log("Grade B");
+}
+else if(mark>=70)
+    {
+    console.log("Grade C");
+}
+else if(mark>=60)
+    console.log("Grade D");
+    {
+}
---
+++ Chapter7_if_else/Task1.js
@@ -0,0 +1,9 @@
+let x=7;
+if(x%2===0)
+    {
+        console.log( x+ " is Even");
+    }
+else 
+    {
+        console.log( x+ "is odd");
+    }
